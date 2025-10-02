a56b789a85d14e23b1ee3b8631fc3e190f8484d4
Jeimy Vasquez <2024161429@ub.edu.bz>
2025-10-02 16:22:36 +0000

    Implemented main test

--- main.cpp
+++ main.cpp
@@ -96,3 +96,30 @@ void freeTree(BSTNode* root) {
     delete root;
 }
 
+int main() {
+    BSTNode* root = nullptr;
+
+    insertRecursive(root, 8);
+    insertIterative(root, 3);
+    insertIterative(root, 10);
+    insertRecursive(root, 1);
+    insertRecursive(root, 6);
+    insertIterative(root, 14);
+    insertRecursive(root, 4);
+    insertIterative(root, 7);
+    insertRecursive(root, 13);
+
+    cout << "Inorder traversal: ";
+    inorderPrint(root);
+    cout << "\n";
+
+    cout << "Search 7 (rec): " <<
+        (searchRecursive(root,7)?"Found":"Not Found") << "\n";
+    cout << "Search 9 (it): "  << 
+        (searchIterative(root,9)?"Found":"Not Found") << "\n";
+
+   // Always free dynamically allocated memory
+    freeTree(root);
+
+    return 0;
+}
